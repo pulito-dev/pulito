edcc0beb4d17f0e785f0a284ae4fa670cca4586d
isakal <sakalivan4@gmail.com>
2024-11-16 19:03:22 +0100

    Clear the repo to add the submodules

--- accommodation_api/routes/deps.py
+++
@@ -1,7 +0,0 @@
-from sqlmodel import Session
-from ..core.db  import db_cl
-from collections.abc import Generator
-
-def get_db() -> Generator[Session, None, None]:
-    with Session(db_cl.engine) as session:
-        yield session
